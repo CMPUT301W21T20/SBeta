897cbcb341b970d37ff47a59afc0b1d9fabf4910
Hao Liu <70818790+liuh9@users.noreply.github.com>
2021-03-16 01:38:22 -0600

    questions feature

--- app/src/main/java/com/example/sbeta/addQuestion.java
+++ app/src/main/java/com/example/sbeta/addQuestion.java
@@ -3,21 +3,29 @@ package com.example.sbeta;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 public class addQuestion extends AppCompatActivity {
     TextView addQuesTittle;
+    EditText questionName;
     EditText quesContent;
     Button quesCancel;
     Button quesConfirm;
@@ -35,8 +43,11 @@ public class addQuestion extends AppCompatActivity {
         quesContent = findViewById(R.id.quesText);
         quesCancel = findViewById(R.id.quesCancel);
         quesConfirm = findViewById(R.id.quesOk);
+        questionName = findViewById(R.id.questionName_context);
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         final CollectionReference collectionReference = db.collection("experiments");
+        final DocumentReference experiment = collectionReference.document(title);
 
         quesCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,11 +59,31 @@ public class addQuestion extends AppCompatActivity {
         quesConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                CollectionReference experiments = experiment.collection("questions");
+                HashMap<String, Object> question_to_add = new HashMap<>();
+                String name = questionName.getText().toString();
                 String content = quesContent.getText().toString();
 
-                collectionReference.document("questions");
+                question_to_add.put("content", content);
 
-
+                experiments
+                        .document(name)
+                        .set(question_to_add)
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("false message", "question cannot be added" + e.toString());
+                            }
+                        })
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("success message", "question added successfully");
+                            }
+                        });
+                onBackPressed();
+                // String content = quesContent.getText().toString();
             }
         });
 
